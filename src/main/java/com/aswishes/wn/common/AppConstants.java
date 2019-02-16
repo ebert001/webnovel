1319f15fd8bcbe1050e52766cc000a79ef3e100f
ebo <ebert_li@163.com>
2019-02-16 11:48:51 +0800

    增加SHA256常量

--- src/main/java/com/aswishes/wn/common/AppConstants.java
+++ src/main/java/com/aswishes/wn/common/AppConstants.java
@@ -6,4 +6,6 @@ import java.nio.charset.StandardCharsets;
 public class AppConstants {
 	/** 项目通用编码类型 */
 	public static final Charset CHARSET_UTF_8 = StandardCharsets.UTF_8;
+	
+	public static final String ALG_SHA256 = "SHA256";
 }
