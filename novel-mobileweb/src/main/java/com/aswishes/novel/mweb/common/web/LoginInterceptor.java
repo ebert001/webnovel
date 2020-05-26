fe6c0817cb1bc0af6b029d131e05ac4d3d6b02ce
ebert <ebert_li@163.com>
2020-05-26 10:57:29 +0800

    update0526

---
+++ novel-mobileweb/src/main/java/com/aswishes/novel/mweb/common/web/LoginInterceptor.java
@@ -0,0 +1,36 @@
+package com.aswishes.novel.mweb.common.web;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.aswishes.novel.core.model.MUser;
+
+public class LoginInterceptor implements HandlerInterceptor {
+	protected static final Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+	
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object obj, ModelAndView mv)
+			throws Exception {
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object obj) throws Exception {
+		MUser user = SessionUtils.getUser();
+		if (user == null) {
+			response.sendRedirect(request.getContextPath() + "/");
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ novel-mobileweb/src/main/java/com/aswishes/novel/mweb/common/web/SessionUtils.java
@@ -0,0 +1,11 @@
+package com.aswishes.novel.mweb.common.web;
+
+import com.aswishes.novel.core.model.MUser;
+
+public class SessionUtils {
+	
+	
+	public static MUser getUser() {
+		return null;
+	}
+}
